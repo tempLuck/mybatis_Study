87b30ec3a8119b6e3965c9292e2ba768f18731da
Hugo <liu9801@foxmail.com>
2020-04-07 19:02:14 +0800

    基于注解入门案例
    @select(select * from user)
    配置查询

--- src/main/java/cn/Tony/Dao/UserDao.java
+++ src/main/java/cn/Tony/Dao/UserDao.java
@@ -2,6 +2,7 @@ package cn.Tony.Dao;
 
 
 import cn.Tony.domain.User;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -12,6 +13,8 @@ public interface UserDao {
      * 查询所有
      * @return
      */
+
+    @Select("select * from user")
     List<User> findAll();
 
 }
